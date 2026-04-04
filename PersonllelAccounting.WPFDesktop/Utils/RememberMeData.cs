0ef88da687a3a9bd22335f4dda769169a9fc0662
Dima <dima@da>
2026-04-04 10:55:47 +0500

    Small fixes

--- PersonllelAccounting.WPFDesktop/Utils/RememberMeData.cs
+++ PersonllelAccounting.WPFDesktop/Utils/RememberMeData.cs
@@ -2,10 +2,10 @@
 
 public class RememberMeData(string login)
 {
-    private static DateTime CreateExpireDate() => DateTime.Now.AddDays(1);
+    private static DateTime CreateExpireDate() => DateTime.Now.AddSeconds(10);
 
     public string Login { get; } = login;
-    public DateTime ExpireDate { get; private set; } = CreateExpireDate();
+    public DateTime ExpireDate { get; set; } = CreateExpireDate();
 
     public bool IsLogged => ExpireDate > DateTime.Now;
 
